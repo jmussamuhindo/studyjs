68173cb34f752a9d526f7fd480601226f74da1c4
Mussa Justin <jmussamuhindo@gmail.com>
2023-02-17 10:42:49 +0100

    Added function

---
+++ declaration.js
@@ -0,0 +1,12 @@
+function helloSay(){
+
+console.log("===========================");
+console.log("Hello word");
+console.log("============================");
+}
+//helloSay()
+
+let a=helloSay;
+a();
+a();
+a();
--- firstApp.js
+++ firstApp.js
@@ -1,4 +1,6 @@
 let a=3;
 let b=4;
 let c=a+b;
+let d=a*b;
 console.log(c)
+console.log(d)
